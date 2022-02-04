6161a1cb8a7bd7647c4b9c95fd66263be9bdf0fc
Will Noland <therewillbnoland@gmail.com>
2022-02-03 18:07:01 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        le = min(strs, key=len)
+        result = ''
+        count = 0
+        paletter = True
+        for p in range(len(le)):
+            container = ''
+            if paletter == True:
+                for i in range(len(strs)):
+                    if strs[i][p] == le[p]:
+                        if le[p] not in container:
+                            container += le[p]
+                    else:
+                        paletter = False
+                        # container = ''
+                        # break
+                        return result
+                result += container
+        return result
